0ca60c0b5af2c502420f040daf0be4c0cb53a738
h3rb <hgilliland@gmail.com>
2018-04-20 12:25:11 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -3,14 +3,12 @@ require('./drawing.js');
 
 // Customize this file to set up your "game engine"
 
-var engine = null;
-
-function resizeEvent() { if ( engine != null ) engine.OnResize(); }
-
 class Engine {
 
  constructor() {
   this.Init();
+  this.frametime = 30.0/1000.0;
+  this.time = 0.0;
  }
 
  InitEvents() {
@@ -31,6 +29,7 @@ class Engine {
   this.InitControls();
   this.InitRenderer();
   this.OnResize();
+  this.EngineSetup();
  }
 
 
@@ -64,19 +63,22 @@ class Engine {
 
 //  this.ImportTileset();
  InitRenderer() {
-  this.renderer = new PIXI.autoDetectRenderer(this.width,this.height,{
-   backgroundColor:0x000000
+//  this.app = new PIXI.Application();
+  this.app = new PIXI.Application({
+   backgroundColor:0x900000
   });
-  document.body.appendChild(this.renderer.view);
+  this.renderer = this.app.renderer;
+  document.body.appendChild(this.app.view); // Add the view to the DOM
   this.stage = new PIXI.Container();
-//  this.zlayer=new PIXI.DisplayGroup(0,function(tile){
-//   tile.zOrder = -tile.position.y;
-//  });
-//  this.uilayer=new PIXI.DisplayGroup(1,false);
+  this.zlayer=new PIXI.DisplayGroup(0,function(tile){
+   tile.zOrder = -tile.position.y;
+  });
+  this.uilayer=new PIXI.DisplayGroup(1,false);
   this.renderer.view.style.position = "absolute";
   this.renderer.view.style.display = "block";
   this.renderer.autoResize = false;
   this.renderer.resize(window.innerWidth, window.innerHeight);
+  console.log(window.innerWidth+"x"+window.innerHeight);
   this.scrolled = { x:0, y:0 };
   this.tilepicker={ };
  }
@@ -89,6 +91,7 @@ class Engine {
  OnResize() {
   this.GetWindowSize();
   this.renderer.resize(this.width,this.height);
+  console.log(this.width+"x"+this.height);
   if ( !this.renderer ) return;
   this.isWebGL = this.renderer instanceof PIXI.WebGLRenderer
   if (!this.isWebGL) {
@@ -106,10 +109,57 @@ class Engine {
   console.log("Delayed Init");
  }
 
- EngineUpdate() {
+EngineSetup() {
+ console.log("Engine Setup");
+ // Replace this with whatever...
+ this.sprites = new PIXI.particles.ParticleContainer(10000, {     scale: true,     position: true,     rotation: true,     uvs: true,     alpha: true });
+ this.entities = [];  // create an array to store all the sprites
+ this.totalSprites = this.renderer instanceof PIXI.WebGLRenderer ? 10 : 10;
+ for (var i = 0; i < this.totalSprites; i++) {
+     // create a new Sprite
+     var dude = PIXI.Sprite.fromImage('./bunny.png');
+     dude.tint = Math.random() * 0xE8D4CD;
+     dude.anchor.set(0.5);
+     dude.scale.set(0.8 + Math.random() * 0.3);
+     dude.x = Math.random() * this.width;
+     dude.y = Math.random() * this.height;
+     dude.tint = Math.random() * 0x808080;
+     dude.direction = Math.random() * Math.PI * 2;
+     dude.turningSpeed = Math.random() - 0.8;
+     dude.speed = (2 + Math.random() * 2) * 0.2;
+     dude.offset = Math.random() * 100;
+     this.entities.push(dude);
+     this.sprites.addChild(dude);
  }
+ this.stage.addChild(this.sprites);
 
- EngineBetween() {
+ this.app.ticker.add(function(delta) {
+//   if ( engine ) console.log("Tick: "+engine.time+" delta: "+delta);
+   var boundsPadding = 100;
+   var boundbox = new PIXI.Rectangle( -boundsPadding,  -boundsPadding,  engine.width + boundsPadding * 2, engine.height + boundsPadding * 2 );
+     //// Replace this with whatever you'd like
+       for (var i = 0; i < engine.entities.length; i++) { // iterate through the sprites and update their position
+           var dude = engine.entities[i];
+           dude.scale.y = 0.95 + Math.sin(engine.time + dude.offset) * 0.05;
+           dude.direction += dude.turningSpeed * 0.01;
+           dude.x += Math.sin(dude.direction) * (dude.speed * dude.scale.y);
+           dude.y += Math.cos(dude.direction) * (dude.speed * dude.scale.y);
+           dude.rotation = -dude.direction + Math.PI;
+           // wrap xy world
+           if (dude.x < boundbox.x) dude.x += boundbox.width;
+           else if (dude.x > boundbox.x + boundbox.width) dude.x -= boundbox.width;
+           if (dude.y < boundbox.y) dude.y += boundbox.height;
+           else if (dude.y > boundbox.y + boundbox.height) dude.y -= boundbox.height;
+       }
+       engine.time += engine.frametime;  // 30ms tick-time
+      ////
+});
+
+ ///
+
+}
+
+ EngineUpdate() {
  }
 
  EngineRender() {
@@ -120,7 +170,6 @@ class Engine {
 
 function Loop() {
  engine.EngineUpdate();
- engine.EngineBetween();
  engine.EngineRender();
  requestAnimationFrame(Loop);
 }
@@ -135,3 +184,8 @@ function Go() {
  setTimeout(function(){engine.DelayedInit();},1500);
  console.log(engine);
 }
+
+
+var engine = new Engine;
+
+function resizeEvent() { if ( engine != null ) engine.OnResize(); }
